81d8ff211f88e98bd36e07a062ca8e76ffa77b7f
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-03-08 16:36:42 +0800

    update gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,19 @@ var less = require('gulp-less');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var nano = require('gulp-cssnano');
+var replaceName = require('gulp-replace-name');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
 gulp.task('default', ['less', 'js', 'watch']);
 
 gulp.task('less', function() {
   return gulp.src('./src/less/fo-popover.less')
     .pipe(less())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(nano())
+    .pipe(replaceName(/\.css/g, '.min.css'))
     .pipe(gulp.dest('./dist/css'));
 });
 
@@ -20,6 +27,9 @@ gulp.task('js', function() {
     .bundle()
     .pipe(source('main.js'))
     .pipe(rename('fo-popover.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(streamify(uglify()))
+    .pipe(streamify(replaceName(/\.js/g, '.min.js')))
     .pipe(gulp.dest('./dist/js'));
 });
 
